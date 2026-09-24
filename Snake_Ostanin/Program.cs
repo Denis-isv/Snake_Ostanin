3c2b36dabfb3c9af212ad46526cbd0e0d90d9e0a
den4ikDSM <denisostain08@gmail.com>
2026-09-24 17:08:23 +0500

    Созданный метод загрузки данных о результатах игроков. Останин

--- Snake_Ostanin/Program.cs
+++ Snake_Ostanin/Program.cs
@@ -295,6 +295,21 @@ namespace Snake_Ostanin
             SW.WriteLine(json);
             SW.Close();
         }
+        public static void LoadLeaders()
+        {
+            if (File.Exists("./leaders.txt"))
+            {
+                StreamReader SR = new StreamReader("./leaders.txt");
+                string json = SR.ReadLine();
+                SR.Close();
+                if (!string.IsNullOrEmpty(json))
+                    Leaders = JsonConvert.DeserializeObject<List<Leaders>>(json);
+                else
+                    Leaders = new List<Leaders>();
+            }
+            else
+                Leaders = new List<Leaders>();
+        }
 
     }
 }
