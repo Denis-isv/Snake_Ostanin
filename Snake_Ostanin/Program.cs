c16ad8e30a6204ff5384184707244845bb810b06
den4ikDSM <denisostain08@gmail.com>
2026-09-24 16:23:46 +0500

    Добавление переменные сервера. Останин

--- Snake_Ostanin/Program.cs
+++ Snake_Ostanin/Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 
 namespace Snake_Ostanin
 {
-    internal class Program
+    class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGame> viewModelGames = new List<ViewModelGame>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
         }
